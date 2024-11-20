66de009432370630d74e1566f71826b3df185f9b
agasgonia <andrea.gasgonia@outlook.com>
2024-11-20 04:22:46 -0500

    changing footer + added map
    the google maps and forms are buggy on my end. need someone to take a look at this to makesure its not just me thank you

--- script.js
+++ script.js
@@ -25,3 +25,29 @@ $(function () {
 	$("#footer-placeholder").load("footer.html");
 	// $("#imports-placeholder").load("imports.html");
 });
+
+// map for footer
+
+function initMap() {
+    const map = new google.maps.Map(document.getElementById('map'), {
+      center: { lat: 40.9984, lng: -75.2207 },
+      zoom: 17,
+      mapId: 'DEMO_MAP_ID',
+    });
+  
+    const marker = new google.maps.marker.AdvancedMarkerElement({
+      map,
+      position: {lat: 37.4239163, lng: -122.0947209},
+    });
+  
+    marker.addListener('click', ({domEvent, latLng}) => {
+      const {target} = domEvent;
+      // Handle the click event.
+      // ...
+    });
+  }
+
+document.addEventListener("DOMContentLoaded", function() {
+    initMap();
+});
+
